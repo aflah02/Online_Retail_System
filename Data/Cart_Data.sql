d6e5e5ff29f8ba7e447833b15b845e3855998faf
Aflah <72096386+aflah02@users.noreply.github.com>
2022-02-24 19:27:08 +0530

    Changes

--- Data/Cart_Data.sql
+++ Data/Cart_Data.sql
@@ -1,24 +1,43 @@
 create table Cart_Data (
-	Cart_id INT,
+	Unique_id INT, 
+	FOREIGN KEY (Unique_id) REFERENCES User on DELETE CASCADE
 );
+
 CREATE TABLE Items_Contained
 (
-Cart_ID int, 
+Unique_id INT, 
 Product_ID INT,
 Quantity INT, 
-Primary key(Cart_ID , Product_ID, Quantity), 
-foreign key(Cart_ID)
-REFERENCES Cart_Data(Cart_ID)
+Primary key(Unique_id , Product_ID, Quantity), 
+foreign key(Unique_id)
+REFERENCES Cart_Data(Unique_id) on DELETE CASCADE
 );
 
-INSERT INTO Cart_Data(Cart_id) VALUES(1);
-INSERT INTO Cart_Data(Cart_id) VALUES(2);
-INSERT INTO Cart_Data(Cart_id) VALUES(3);
-INSERT INTO Cart_Data(Cart_id) VALUES(4);
-INSERT INTO Cart_Data(Cart_id) VALUES(5);
-INSERT INTO Cart_Data(Cart_id) VALUES(6);
-INSERT INTO Cart_Data(Cart_id) VALUES(7);
-INSERT INTO Cart_Data(Cart_id) VALUES(8);
-INSERT INTO Cart_Data(Cart_id) VALUES(9);
-
+INSERT INTO Cart_Data(Unique_id) VALUES(1);
+INSERT INTO Cart_Data(Unique_id) VALUES(2);
+INSERT INTO Cart_Data(Unique_id) VALUES(3);
+INSERT INTO Cart_Data(Unique_id) VALUES(4);
+INSERT INTO Cart_Data(Unique_id) VALUES(5);
+INSERT INTO Cart_Data(Unique_id) VALUES(6);
+INSERT INTO Cart_Data(Unique_id) VALUES(7);
+INSERT INTO Cart_Data(Unique_id) VALUES(8);
+INSERT INTO Cart_Data(Unique_id) VALUES(9);
+INSERT INTO Cart_Data(Unique_id) VALUES(10);
+INSERT INTO Cart_Data(Unique_id) VALUES(11);
+INSERT INTO Cart_Data(Unique_id) VALUES(12);
+INSERT INTO Cart_Data(Unique_id) VALUES(13);
+INSERT INTO Cart_Data(Unique_id) VALUES(14);
+INSERT INTO Cart_Data(Unique_id) VALUES(15);
+INSERT INTO Cart_Data(Unique_id) VALUES(16);
+INSERT INTO Cart_Data(Unique_id) VALUES(17);
+INSERT INTO Cart_Data(Unique_id) VALUES(18);
+INSERT INTO Cart_Data(Unique_id) VALUES(19);
+INSERT INTO Cart_Data(Unique_id) VALUES(20);
+INSERT INTO Items_Contained VALUES(1, 1, 1);
+INSERT INTO Items_Contained VALUES(1, 1, 1);
+INSERT INTO Items_Contained VALUES(1, 1, 1);
+INSERT INTO Items_Contained VALUES(1, 1, 1);
+INSERT INTO Items_Contained VALUES(1, 1, 1);
+INSERT INTO Items_Contained VALUES(1, 1, 1);
+INSERT INTO Items_Contained VALUES(1, 1, 1);
 INSERT INTO Items_Contained VALUES(1, 1, 1);
