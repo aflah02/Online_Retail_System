40e0189b48b6ea448811e3d578bbd9395deb0fd1
Aryaman Raina <aryaman20034@iiitd.ac.in>
2022-02-24 18:33:14 +0400

    aryaman tables

---
+++ Data/BRAND.sql
@@ -0,0 +1,29 @@
+create table INVENTORY (
+	brand_name VARCHAR(50)
+);
+insert into INVENTORY (brand_name) values ('Amul');
+insert into INVENTORY (brand_name) values ('London Dairy');
+insert into INVENTORY (brand_name) values ('Britannia');
+insert into INVENTORY (brand_name) values ('Nesle');
+insert into INVENTORY (brand_name) values ('Kelloggs');
+insert into INVENTORY (brand_name) values ('FitBit');
+insert into INVENTORY (brand_name) values ('Amway');
+insert into INVENTORY (brand_name) values ('Loreal');
+insert into INVENTORY (brand_name) values ('Nivea');
+insert into INVENTORY (brand_name) values ('Gillette');
+insert into INVENTORY (brand_name) values ('Dove');
+insert into INVENTORY (brand_name) values ('Adidas');
+insert into INVENTORY (brand_name) values ('Nike');
+insert into INVENTORY (brand_name) values ('Puma');
+insert into INVENTORY (brand_name) values ('Gap');
+insert into INVENTORY (brand_name) values ('Gatorade');
+insert into INVENTORY (brand_name) values ('Coca-Cola');
+insert into INVENTORY (brand_name) values ('Dabur India');
+insert into INVENTORY (brand_name) values ('PepsiCo');
+insert into INVENTORY (brand_name) values ('Samsung');
+insert into INVENTORY (brand_name) values ('LG');
+insert into INVENTORY (brand_name) values ('Whirlpool');
+insert into INVENTORY (brand_name) values ('Godrej Interio');
+insert into INVENTORY (brand_name) values ('Apple');
+insert into INVENTORY (brand_name) values ('JBL');
+insert into INVENTORY (brand_name) values ('Haldirams');
