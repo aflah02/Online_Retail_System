db8287a3915fdd38f71223665cd5932512208872
Shivaansh Mital <shivaansh20122@iiitd.ac.in>
2022-02-24 19:24:05 +0530

    Product Category and admin

---
+++ Data/admin.sql
@@ -0,0 +1,10 @@
+create table ADMIN (
+	admin_id INT,
+	username VARCHAR(50),
+	password VARCHAR(50),
+    PRIMARY KEY(admin_id)
+);
+insert into ADMIN (admin_id, username, password) values (1, 'Aryaman', 'Raina1');
+insert into ADMIN (admin_id, username, password) values (2, 'Aflah', 'Khan2');
+insert into ADMIN (admin_id, username, password) values (3, 'Faizan', 'Haider3');
+insert into ADMIN values(4,'Shivaansh','Mital4');
