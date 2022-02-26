0a0585694f368dadde07fcc97058a8901e2ee87d
Shivaansh Mital <shivaansh20122@iiitd.ac.in>
2022-02-26 17:10:19 +0530

    Addition of products and updation of product cost

--- Queries/admin_related.sql
+++ Queries/admin_related.sql
@@ -7,7 +7,7 @@ WHERE O.shipper_id = S.shipper_id AND U.id = O.unique_id;
 UPDATE Inventory I
 SET I.quantity = I.quantity + 10
 WHERE I.product_id = (
-    SELECT product_id from product where product_name = 'Milk' AND brand_name='Amul'
+    SELECT product_id from product where product_name = 'Milk' AND brand_name='London Dairy'
 );
 
 -- list all orders where there exists atleast one product belonging to a particular category (say Electronics)
@@ -24,3 +24,17 @@ SELECT S.shipper_name, S.delivery_speed
 FROM Shipper S
 WHERE S.delivery_speed >= 2;
 
+-- update product cost 
+UPDATE product I
+SET I.product_cost = I.product_cost + 10
+WHERE I.product_name='Milk' and I.brand_name='London Dairy';
+
+-- insert a new product
+DELETE FROM brand WHERE brand_name='Google';
+DELETE FROM product WHERE product_id=46;
+delete from belongsto where product_id=46 and category_id=11;
+delete from inventory where product_id=46;
+insert into brand values ('Google');
+insert into product (product_id, product_name,product_cost ,brand_name) values (46, 'Pixel',20000 ,'Google');
+insert into belongsTo (product_id,category_id) values (46,11);
+insert into INVENTORY (product_id, quantity) values (46, 1);
