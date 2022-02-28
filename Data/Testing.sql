9fd69d8c0e5fb3141a931456578d8a49278f6385
Aflah <72096386+aflah02@users.noreply.github.com>
2022-02-28 17:22:41 +0530

    Test

--- Data/Testing.sql
+++ Data/Testing.sql
@@ -1,9 +1,9 @@
 delete from billing_details where billing_details.billing_id 
-IN (Select billing_id From order_table where order_table.Unique_id = 1) 
+IN (Select billing_id From order_table where order_table.Unique_id = 3) 
 AND EXISTS (Select * From order_table,shipper 
 Where DATEDIFF(CURRENT_DATE, DATE_ADD(order_table.Date_Time, INTERVAL shipper.Delivery_speed DAY)) > 0
 AND order_table.Shipper_id = shipper.shipper_id And order_table.billing_id=billing_details.billing_id);
-delete from user where user.id = 1 AND NOT EXISTS (Select * From order_table,shipper 
+delete from user where user.id = 3 AND NOT EXISTS (Select * From order_table,shipper 
 Where DATEDIFF(CURRENT_DATE, DATE_ADD(order_table.Date_Time, INTERVAL shipper.Delivery_speed DAY)) > 0 
 AND order_table.Shipper_id = shipper.shipper_id);
 SELECT 
@@ -11,4 +11,4 @@ SELECT
 FROM
     order_table
 WHERE
-    Unique_id = 1
+    Unique_id = 3
