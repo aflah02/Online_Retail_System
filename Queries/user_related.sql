14ed9b55c462c8489128cf79139101da6114de1f
Aflah <72096386+aflah02@users.noreply.github.com>
2022-02-28 22:16:32 +0530

    Changed

--- Queries/user_related.sql
+++ Queries/user_related.sql
@@ -1,12 +1,15 @@
 -- Delete User
-SELECT *
-CASE
-    WHEN EXISTS where order_table.Date_Time + shipper.Delivery_speed DAY > CURRENT_DATE; THEN 
-    ELSE delete from billing_details where billing_details.billing_id 
-         IN (Select billing_id From order_table where order_table.Unique_id = 2); 
-         delete from user where user.id = 2; 
-         Select * 
-         From billing_details
-         Where billing_id = 2;
-END
-FROM order_table, shipper;
+delete from billing_details where billing_details.billing_id 
+IN (Select billing_id From order_table where order_table.Unique_id = 3) 
+AND EXISTS (Select * From order_table,shipper 
+Where DATEDIFF(CURRENT_DATE, DATE_ADD(order_table.Date_Time, INTERVAL shipper.Delivery_speed DAY)) > 0
+AND order_table.Shipper_id = shipper.shipper_id And order_table.billing_id=billing_details.billing_id);
+delete from user where user.id = 3 AND NOT EXISTS (Select * From order_table,shipper 
+Where DATEDIFF(CURRENT_DATE, DATE_ADD(order_table.Date_Time, INTERVAL shipper.Delivery_speed DAY)) > 0 
+AND order_table.Shipper_id = shipper.shipper_id);
+SELECT 
+    *
+FROM
+    order_table
+WHERE
+    Unique_id = 3
