266dcd5e2b4a5ef88e27613c3e8a118a35aec860
Aflah <72096386+aflah02@users.noreply.github.com>
2022-02-28 13:09:18 +0530

    Fixes

--- Data/DataDump.sql
+++ Data/DataDump.sql
@@ -216,7 +216,7 @@ insert into category (category_id, category_name, category_info) values (11, 'El
 insert into category (category_id, category_name, category_info) values (12, 'Sports Equipment', 'This category encompasses all you would want to satisfy the athlete within you');
 
 create table Brand (
-	brand_name VARCHAR(50) NOT NULL AUTO_INCREMENT,
+	brand_name VARCHAR(50) NOT NULL,
     primary key(brand_name)
 );
 insert into Brand (brand_name) values ('Amul');
