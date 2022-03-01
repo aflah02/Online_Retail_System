b1479bf7b4f9904b0169ae790a5fd843bfb56a92
Aflah <72096386+aflah02@users.noreply.github.com>
2022-03-01 12:59:15 +0530

    Done

--- Queries/cart_related.sql
+++ Queries/cart_related.sql
@@ -11,16 +11,16 @@ where P.product_id=I.Product_ID and I.Unique_id=U.id and P.product_id IN (select
 group by Temp.Username;
 
 
---empty cart for a particular user
+-- empty cart for a particular user
 delete from items_contained where items_contained.Unique_id = 19;
 
---Remove particular product from cart for a particular user
+-- Remove particular product from cart for a particular user
 delete from items_contained where items_contained.Unique_id = 19 and items_contained.product_id = 20;
 
---add to cart a particular product in a particular quantity to a particular user's cart if order doesn't exist
+-- add to cart a particular product in a particular quantity to a particular user's cart if order doesn't exist
 insert into items_contained values (19,20,5);
 
---add to cart a particular product in a particular quantity to a particular user's cart if order exists
+-- add to cart a particular product in a particular quantity to a particular user's cart if order exists
 
 update  items_contained 
 set quantity = case 
@@ -28,10 +28,10 @@ set quantity = case
     then quantity + 10 
     else quantity  end;
 
---calculate cost of a particular order
+-- calculate cost of a particular order
 select order_id as "Order Number", sum(items_purchased.quantity * (select product_cost from product where product.product_id = items_purchased.product_id )) from items_purchased where items_purchased.order_id = 1;
 
---cancel an order
+-- cancel an order
 update inventory set quantity = quantity + (select quantity from items_purchased where 
 order_id = 2 and inventory.product_id = items_purchased.product_id) where product_id in (select product_id from items_purchased where order_id = 2);
 
@@ -41,12 +41,10 @@ delete from order_table where order_id = 2;
 -- buy now
 -- update inventory, update billing, insert into order, remove from cart
 -- you have cart_id and Unique_id
-
-
-
 update inventory set quantity = quantity - (select quantity from items_contained 
 where inventory.product_id = items_contained.product_id and items_contained.Unique_id = 1)
 where product_id in (select product_id from items_contained where Unique_id = 1);
+
 -- inventory updated
 
 insert into billing_details (payment_mode,billing_address) values ('Net Banking','East Avenue, New York');
@@ -61,7 +59,7 @@ insert into items_purchased (31,8,2);
 delete from items_contained where Unique_id = 1;
 
 -- deleted from cart
---buy now done
+-- buy now done
 
 -- all orders with cost < average 
  select * from order_table where (select Order_id from items_purchased where cost < (select avg(cost) from items_purchased group by (Order_id)));
