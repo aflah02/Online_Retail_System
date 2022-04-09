5caf7d8b843ce26fd772f914b47324ae43fb9002
Shivaansh Mital <shivaansh20122@iiitd.ac.in>
2022-04-09 18:53:43 +0530

    APIs working mostly done all on my part some of relations remaining

--- APIs/shivaansh_APIs.py
+++ APIs/shivaansh_APIs.py
@@ -5,7 +5,7 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="1234",
+    passwd="22510FamSuccess",
     database = 'retaildb')
 
 app = flask.Flask(__name__)
@@ -27,7 +27,7 @@ def addProducts(name,brandname,cost):
 def deleteProduct(name,brandname):
     try:
         c=db.cursor()
-        c.execute(f"delete from product where product.name={name} and product.brand_name={brandname}")
+        c.execute(f"delete from product where product_name='{name}' and product.brand_name='{brandname}'")
         db.commit()
         return "Success"
     except:
@@ -38,7 +38,7 @@ def deleteProduct(name,brandname):
 def addBrand(name):
     try:
         cursor=db.cursor()
-        cursor.execute("insert into brand (brand_name) values(%s)" ,(name))
+        cursor.execute(f"insert into brand (brand_name) values('{name}')")
         db.commit()
         return "Success"
     except:
@@ -48,20 +48,20 @@ def addBrand(name):
 #API for deleting brand
 @app.route('/deleteBrand/<string:brandname>')
 def deleteBrand(brandname):
-    # try:
-    c=db.cursor()
-    c.execute(f"delete from brand where brand_name={brandname}")
-    db.commit()
-    return "Success"
-    # except:
-    #     return "Error"
+    try:
+        c=db.cursor()
+        c.execute(f"delete from brand where brand_name='{brandname}'")
+        db.commit()
+        return "Success"
+    except:
+        return "Error"
 
 #list all products 
 @app.route('/listAllProducts')
 def listAllProducts():
     try:
         c=db.cursor()
-        c.execute("Select * from product")
+        c.execute("Select product_name, product_cost,brand_name from product")
         result = c.fetchall()
         return flask.jsonify(result)
     except:
@@ -81,13 +81,70 @@ def listAllBrands():
 #API for searching using product name
 @app.route('/searchUsingProductName/<string:name>')
 def searchUsingProductName(name):
-    # try:
-    c=db.cursor()
-    c.execute(f"Select * from product where product_name={name}and exists(select product_id from inventory where inventory.product_id = product.product_id and inventory.quantity>0)")
-    result = c.fetchall()
-    return flask.jsonify(result)
-    # except:
-    #     return "Error"
+    try:
+        c=db.cursor()
+        c.execute(f"Select product_name, product_cost,brand_name from product where product_name='{name}' and exists (select product_id from inventory where inventory.product_id = product.product_id and inventory.quantity>0)")
+        result = c.fetchall()
+        return flask.jsonify(result)
+    except:
+        return "Error"
+
+
+#API for searching using brand name
+@app.route('/searchUsingBrandName/<string:name>')
+def searchUsingBrandName(name):
+    try:
+        c=db.cursor()
+        c.execute(f"Select product_name, product_cost,brand_name from product where brand_name='{name}' and exists (select product_id from inventory where inventory.product_id = product.product_id and inventory.quantity>0)")
+        result = c.fetchall()
+        return flask.jsonify(result)
+    except:
+        return "Error"
+
+#list out category information
+@app.route('/displayCategories')
+def displayCategories():
+    try:
+        c=db.cursor()
+        c.execute(f"select category_name,category_info from category ")
+        result = c.fetchall()
+        return flask.jsonify(result)
+    except:
+        return "Error"
+
+
+# search products using category name
+@app.route('/searchUsingCategoryName/<string:name>')
+def searchUsingCategoryName(name):
+    try:
+        c=db.cursor()
+        c.execute(f"select product_name,product_cost,brand_name from product natural join belongsto where category_id IN (select category_id from category where category_name='{name}') and exists (select product_id from inventory where inventory.product_id = product.product_id and inventory.quantity>0)")
+        result = c.fetchall()
+        return flask.jsonify(result)
+    except:
+        return "Error"
+
+#add category information
+@app.route('/addCategory/<string:name>/<string:info>')
+def addCategory(name,info):
+    try:
+        cursor=db.cursor()
+        cursor.execute(f"insert into category(category_name, category_info) values('{name}','{info}')")
+        db.commit()
+        return "Success"
+    except:
+        return "Error"
+
+#API for deleting category
+@app.route('/deleteCategory/<string:categoryname>')
+def deleteCategory(categoryname):
+    try:
+        c=db.cursor()
+        c.execute(f"delete from category where category_name='{categoryname}'")
+        db.commit()
+        return "Success"
+    except:
+        return "Error"
 
 if __name__ == '__main__':
     app.run(debug=True)
