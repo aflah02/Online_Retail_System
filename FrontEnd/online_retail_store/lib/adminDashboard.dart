22a52d82deb2109fe2cad6190fa7bdd1c2cf1a30
Faizan Haider <faizan20083@iiitd.ac.in>
2022-04-13 09:52:01 +0530

    SideBar

--- FrontEnd/online_retail_store/lib/adminDashboard.dart
+++ FrontEnd/online_retail_store/lib/adminDashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'sidebar.dart';
 
 class dashboard extends StatefulWidget {
   const dashboard({Key? key}) : super(key: key);
@@ -11,6 +12,10 @@ class _dashboardState extends State<dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Admin Page"),
+      ),
+      drawer: Sidebar(),
       backgroundColor: Colors.teal,
       body: SafeArea(
           child: Column(
