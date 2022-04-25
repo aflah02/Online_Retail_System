d4e2a232c0cefc277cb7a8a095353aa6506cdc7c
Faizan Haider <faizan20083@iiitd.ac.in>
2022-04-25 17:14:22 +0530

    Search

--- FrontEnd/online_retail_store/lib/search.dart
+++ FrontEnd/online_retail_store/lib/search.dart
@@ -10,18 +10,31 @@ class Product {
   late String name;
   late String url;
 
-  Product(
-      {required this.name,
-      required this.price,
-      required this.brand,
-      required this.url});
+  Product({
+    required this.name,
+    required this.price,
+    required this.brand,
+  });
 }
 
 class Search extends SearchDelegate<String> {
-  Future<Product> getItem() async {
-    var data = await http.get(Uri.parse(''));
-    Product temp = Product(brand: '', name: '', price: '', url: '');
-    return temp;
+  Future<List<Product>> getItem(String name) async {
+    List<Product> ret = [];
+    var data = await http.get(Uri.parse(
+        "http://127.0.0.1:5000/getAllProductsMatchingName/'" + name + "'"));
+    var jsonData = json.decode(data.body);
+
+    for (var prod in jsonData) {
+      Product temp = Product(name: prod[1], price: prod[2], brand: prod[3]);
+      var img = await http.get(Uri.parse(
+          'http://127.0.0.1:5000/getProductImage/' + prod[1] + '/' + prod[3]));
+
+      var link = json.decode(img.body);
+      temp.url = link;
+      ret.add(temp);
+    }
+
+    return ret;
   }
 
   List<String> categories = [
@@ -92,6 +105,123 @@ class Search extends SearchDelegate<String> {
     "Wilson",
     "Yonex"
   ];
+
+  Widget generateCards(String str) {
+    return Container(
+      height: 325,
+      child: FutureBuilder(
+        future: getItem(str),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData) {
+            return Container(
+              child: Center(
+                child: Column(children: [
+                  Text("Loading ..."),
+                  ElevatedButton(
+                      onPressed: () {
+                        print('here');
+                        getItem(str);
+                        print(snapshot.data);
+                      },
+                      child: Text("Loading ..."))
+                ]),
+              ),
+            );
+          } else {
+            return (ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                    height: 290,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                      color: Colors.white,
+                    ),
+                    margin: EdgeInsets.fromLTRB(15, 0, 15, 5),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(50, 0, 0, 20),
+                          child: Image.network(
+                            snapshot.data[index].url,
+                            height: 120,
+                            width: 140,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(7),
+                          child: Text(
+                            '${snapshot.data[index].name}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                          child: Text('${snapshot.data[index].brand}'),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                '${snapshot.data[index].price}' + '₹',
+                                style: TextStyle(
+                                  color: Colors.black87,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+
+                              // IconButton(
+                              //     onPressed: () {},
+                              //     icon: Icon(Icons.add_shopping_cart))
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          margin: EdgeInsets.fromLTRB(75, 0, 0, 0),
+                          child: ElevatedButton.icon(
+                            onPressed: () {},
+                            icon: Icon(Icons.shopping_cart),
+                            label: Text('Add to cart'),
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  Color.fromRGBO(162, 124, 91, 0.7)),
+                            ),
+                          ),
+                        )
+                      ],
+                    ));
+              },
+            ));
+            // return Container(
+            //   child: Center(
+            //     child: FloatingActionButton(
+            //       onPressed: () {
+            //         print(snapshot);
+            //       },
+            //       child: Text("Print data"),
+            //     ),
+            //   ),
+            // );
+          }
+        },
+      ),
+    );
+  }
+
   List<String> searches = [
     "Aashirvaad Atta with Multi Grains",
     "Aashirvaad Select Premium Sharbati Atta, 5kg",
@@ -350,84 +480,7 @@ class Search extends SearchDelegate<String> {
     } else if (brands.contains(query)) {
       return BrandPage(name: query);
     } else if (searches.contains(query)) {
-      String url;
-      String brand;
-      String name;
-      String price;
-
-      return Container(
-          height: 290,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(15)),
-            color: Colors.white,
-          ),
-          margin: EdgeInsets.fromLTRB(15, 0, 15, 5),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(50, 0, 0, 20),
-                // child: Image.network(
-                //   snapshot.data[index].url,
-                //   height: 120,
-                //   width: 140,
-                // ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(7),
-                // child: Text(
-                //   '${snapshot.data[index].name}',
-                //   style: TextStyle(
-                //     fontWeight: FontWeight.bold,
-                //     fontSize: 16,
-                //   ),
-                // ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
-                // child: Text('${snapshot.data[index].brand}'),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      // '${snapshot.data[index].price}' + '₹',
-                      '',
-                      style: TextStyle(
-                        color: Colors.black87,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
-                    ),
-
-                    // IconButton(
-                    //     onPressed: () {},
-                    //     icon: Icon(Icons.add_shopping_cart))
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(75, 0, 0, 0),
-                child: ElevatedButton.icon(
-                  onPressed: () {},
-                  icon: Icon(Icons.shopping_cart),
-                  label: Text('Add to cart'),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        Color.fromRGBO(162, 124, 91, 0.7)),
-                  ),
-                ),
-              )
-            ],
-          ));
+      return generateCards(query);
     } else
       return Container(
         child: Center(
