60ef009d4d00149eba8a487b880810ee3eaa24cd
Aflah <72096386+aflah02@users.noreply.github.com>
2022-04-30 17:48:47 +0530

    Fix

--- APIs/backend.py
+++ APIs/backend.py
@@ -535,12 +535,6 @@ def deleteUser(userID):
             delete from user where user.id = {userID} AND NOT EXISTS (Select * From order_table,shipper 
             Where DATEDIFF(CURRENT_DATE, DATE_ADD(order_table.Date_Time, INTERVAL shipper.Delivery_speed DAY)) > 0 
             AND order_table.Shipper_id = shipper.shipper_id);
-            SELECT 
-                *
-            FROM
-                order_table
-            WHERE
-                Unique_id = {userID}
         """)
         db.commit()
         db.close()
